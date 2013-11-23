c60c9cb60d142bb195a6dd4b0b61a6131302b903
mikej100 <mike.jennings@chorleywood.uk.net>
2013-11-23 01:26:58 +0000

    Add directories to repo which were missing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsapp20131120b::Application.config.secret_key_base = '57b54a9e967d4759ee474411fe6289948531ad3ca4c81b608990ebd5a0617b74270d2532290c21878953bd2ac54dda7a64733c2c9b49e40120f379ec0e1fe4f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsapp20131120b::Application.config.session_store :cookie_store, key: '_railsapp_20131120b_session'
